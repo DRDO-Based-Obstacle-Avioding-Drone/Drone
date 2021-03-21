dca26643b1eb2fc8bc1eb2d217e37929c52b52c8
aniketRaj01 <72849128+aniketRaj01@users.noreply.github.com>
2021-03-21 07:22:36 +0530

    Repo created, world 1 complete

---
+++ scripts/main_controller.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+
+"""
+set_attitude_target.py: (Copter Only)
+This example shows how to move/direct Copter and send commands
+ in GUIDED_NOGPS mode using DroneKit Python.
+Caution: A lot of unexpected behaviors may occur in GUIDED_NOGPS mode.
+        Always watch the drone movement, and make sure that you are in dangerless environment.
+        Land the drone as soon as possible when it shows any unexpected behavior.
+Tested in Python 2.7.10
+"""
+
+
+
+import time
+import math
+from sensor_msgs.msg import NavSatFix
+from std_msgs.msg import Float32
+# from vitarana_drone.msg import *
+# from pid_tune.msg import PidTune
+from sensor_msgs.msg import Imu
+from std_msgs.msg import Float32
+import rospy
+import time
+import tf
+from numpy import zeros
+from collections import deque
+from sensor_msgs.msg import PointCloud2
+from sensor_msgs import point_cloud2
+import rospy
+import time
+
+
+
+
+
+def lat_to_x(input_latitude):
+    return 110692.0702932625*(input_latitude+35.3632591)
+def long_to_x(input_longitude):
+    return -105292.0089353767*(input_longitude-149.1653452)
+
+def x_to_lat(input_x):
+    return input_x/110692.0702932625 - 35.3632591
+def x_to_long(input_x):
+    return input_x/(-105292.0089353767) + 149.1653452
+
+offset_x  = 50
+offset_y = 0
+maze = zeros([101,101],int)
+
+def astar(maze,start,end):
+    parent=zeros([101,101,2],int)
+    vis=zeros([101,101],int)
+    insertq=zeros([101,101],int)
+    for i in range(100):
+        for j in range(100):
+            parent[i][j][0]=parent[i][j][1]=-1
+
+    q=deque()
+    q.append(start)
+    val=start
+    insertq[start[0]][start[1]]=1
+
+    while q:
+        temp=q.popleft()
+        vis[temp[0]][temp[1]]=1
+        if temp[0] == end[0] and temp[1] == end[1]:
+            break
+        if temp[0]+1<100 and insertq[temp[0]+1][temp[1]]==0 and maze[temp[0]+1][temp[1]]==0:
+            insertq[temp[0]+1][temp[1]]=1
+            q.append([temp[0]+1,temp[1]])
+            parent[temp[0]+1][temp[1]][0]=temp[0]
+            parent[temp[0]+1][temp[1]][1]=temp[1]
+        if temp[1]+1<100 and insertq[temp[0]][temp[1]+1]==0 and maze[temp[0]][temp[1]+1]==0:
+            insertq[temp[0]][temp[1]+1]=1
+            q.append([temp[0],temp[1]+1])
+            parent[temp[0]][temp[1]+1][0]=temp[0]
+            parent[temp[0]][temp[1]+1][1]=temp[1]
+        if temp[0]-1>=0 and insertq[temp[0]-1][temp[1]]==0 and maze[temp[0]-1][temp[1]]==0:
+            insertq[temp[0]-1][temp[1]]=1
+            q.append([temp[0]-1,temp[1]])
+            parent[temp[0]-1][temp[1]][0]=temp[0]
+            parent[temp[0]-1][temp[1]][1]=temp[1]
+        if temp[1]-1>=0 and insertq[temp[0]][temp[1]-1]==0 and maze[temp[0]][temp[1]-1]==0:
+            insertq[temp[0]][temp[1]-1]=1
+            q.append([temp[0],temp[1]-1])
+            parent[temp[0]][temp[1]-1][0]=temp[0]
+            parent[temp[0]][temp[1]-1][1]=temp[1]
+    
+    temp=[end[0],end[1]]
+    q1=deque()
+    path=[]
+
+    while temp[0]>=0 :
+        q1.append(temp)
+        temp=parent[temp[0]][temp[1]]
+
+    #****************RETURNING PATH*******************************
+    while q1:
+        temp=q1.pop()
+        path.append(temp)
+
+    return path   
+
+
+def operate_astar(start,end,drone,maze,obs):
+    print(start)
+    print(end)
+    start[0] = (int)(start[0])+50
+    start[1] = (int)(start[1])
+    final_dest = end
+    print(final_dest)
+    end[0] = (int)(end[0])+50
+    end[1] = (int)(end[1])
+    for p in obs:
+        if abs(p[1])<1:
+            maze[(int)(drone[0]+p[0])+50][(int)(drone[1]+p[2])]=1
+            maze[(int)(drone[0]+p[0])+50+1][(int)(drone[1]+p[2])]=1
+
+    print("PRINTING MAZE")
+    for i in range(100):
+        for j in range(100):
+            if maze[i][j] == 1:
+                print(i,j)
+    maze[start[0]][start[1]] = 0
+    path = astar(maze,start,end)
+    print("PATH")
+    print("-------")
+    print(path)
+    print("------")
+    print(len(path))
+    print(final_dest)
+    if(len(path)<=1):
+        return [final_dest[0]-50,final_dest[1]]
+    else:
+        return [path[1][0]-50 , path[1][1]]
+    
+
+
+
+
+class Iris():
+    """docstring for Edrone"""
+    def __init__(self):
+        rospy.init_node('position_controller')
+        self.alt_pub = rospy.Publisher('/Thrust', Float32, queue_size=10)
+        self.lat_pub = rospy.Publisher('/Roll', Float32, queue_size=10)
+        self.long_pub = rospy.Publisher('/Pitch', Float32, queue_size=10)
+        rospy.Subscriber("/mavros/global_position/global", NavSatFix, self.PoseCallBack)
+        rospy.Subscriber('/depth_camera/depth/points', PointCloud2, self.callback_pointcloud)
+        self.postn = [0,0,0]
+        self.set_postn = [0,25,4]
+        self.thrust =0
+        self.roll =0
+        self.pitch=0
+        self.gen = [[0,0,0]]
+
+    def callback_pointcloud(self,data):
+        assert isinstance(data, PointCloud2)
+        self.gen = point_cloud2.read_points(data, field_names=("x", "y", "z"), skip_nans=True)
+        time.sleep(1)
+    # print(data.height,data.width)
+    # assert isinstance(data, PointCloud2)
+    # self.gen = point_cloud2.read_points(data, field_names=("x", "y", "z"), skip_nans=True)
+    # time.sleep(1)
+    # # print type(gen)
+    # # for p in gen:
+    # #     print(p)
+    # #     print " x : %.3f  y: %.3f  z: %.3f" %(p[0],p[1],p[2])
+        
+
+
+
+    def PoseCallBack(self,msg):
+        self.postn[0] = lat_to_x(msg.latitude)
+        self.postn[1] = long_to_x(msg.longitude)
+        self.postn[2] = msg.altitude - 603.35877
+
+
+    def guider(self):
+        self.iter = 0
+        self.lat_iter=0
+        self.long_iter=0
+        self.prev_height_err = 0
+        self.prev_lat_err =0
+        self.prev_long_err =0
+        self.flag = 0
+        self.next_lat = 0
+        self.next_long = 0
+        self.condition = 0
+        self.arr = [[0,0,0]]
+        while(True):
+            global maze
+            if(abs(self.next_lat-self.postn[0])<2 and abs(self.next_long - self.postn[1])<2):
+                time.sleep(3)
+                val = operate_astar([self.next_lat,self.next_long],[self.set_postn[0],self.set_postn[1]],[self.postn[0],self.postn[1]],maze,self.gen)
+                self.next_lat = val[0]
+                self.next_long = val[1]
+            self.alt_pub.publish(self.set_postn[2])
+            self.lat_pub.publish((self.next_lat))
+            self.long_pub.publish((self.next_long))
+            time.sleep(0.1)
+            print(self.postn)
+            print("PUBLISHED->")
+            print(self.next_lat,self.next_long)
+            print(self.set_postn)
+
+            
+            
+
+
+
+
+
+
+
+
+
+#main
+
+if __name__ == "__main__":
+    try:
+        publish_cmd = Iris()
+        time.sleep(1)
+        publish_cmd.guider()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("node terminated.")
+
---
+++ scripts/main_executer.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+"""
+set_attitude_target.py: (Copter Only)
+This example shows how to move/direct Copter and send commands
+ in GUIDED_NOGPS mode using DroneKit Python.
+Caution: A lot of unexpected behaviors may occur in GUIDED_NOGPS mode.
+        Always watch the drone movement, and make sure that you are in dangerless environment.
+        Land the drone as soon as possible when it shows any unexpected behavior.
+Tested in Python 2.7.10
+"""
+
+from dronekit import connect, VehicleMode, LocationGlobalRelative
+from pymavlink import mavutil
+import time
+import math
+import rospy
+from std_msgs.msg import Float32
+import argparse  
+parser = argparse.ArgumentParser()
+parser.add_argument('--connect', default= 'udp:127.0.0.1:14550')
+args = parser.parse_args()
+# Connect to the Vehicle
+print 'Connecting to vehicle on: %s' % args.connect
+vehicle = connect(args.connect, baud=57600, wait_ready=True)
+rospy.init_node('attitude_controller')
+hght = 4
+latd = -35.3632591
+longd =  149.1653452
+
+
+def lat_to_x(input_latitude):
+    return 110692.0702932625*(input_latitude+35.3632591)
+def long_to_x(input_longitude):
+    return -105292.0089353767*(input_longitude-149.1653452)
+def x_to_lat(input_x):
+	return input_x/110692.0702932625 - 35.3632591
+def x_to_long(input_x):
+	return input_x/(-105292.0089353767) + 149.1653452
+
+
+def demo(data):
+    global hght
+    hght = data.data
+   
+
+def demo2(data):
+	global latd
+	latd = x_to_lat(data.data)
+	# print("\nsubscribedlat->",data.data, '   ', latd,'\n\n')
+	
+    
+
+def demo3(data):
+	global longd
+	longd = x_to_long(data.data)
+	# print("\nsubscribedlong->",data.data, '   ', longd,'\n\n')
+    
+
+rospy.Subscriber('/Thrust', Float32, demo)
+rospy.Subscriber('/Roll', Float32, demo2)
+rospy.Subscriber('/Pitch', Float32, demo3)
+
+
+
+# Function to arm and then takeoff to a user specified altitude
+def arm_and_takeoff(aTargetAltitude):
+
+	print "Basic pre-arm checks"
+	# Don't let the user try to arm until autopilot is ready
+	while not vehicle.is_armable:
+		print " Waiting for vehicle to initialise..."
+		time.sleep(1)
+
+	print "Arming motors"
+	# Copter should arm in GUIDED mode
+	vehicle.mode    = VehicleMode("GUIDED")
+	vehicle.armed   = True
+
+	while not vehicle.armed:
+		print " Waiting for arming..."
+		time.sleep(1)
+
+	print "Taking off!"
+	vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+	# Check that vehicle has reached takeoff altitude
+	while True:
+		print " Altitude: ", vehicle.location.global_relative_frame.alt 
+		#Break and return from function just below target altitude.        
+		if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
+			print "Reached target altitude"
+			break
+		time.sleep(1)
+
+
+if __name__ == '__main__':
+#     vehicle.mode = VehicleMode("LAND")
+
+# # Close vehicle object
+# vehicle.close()
+    arm_and_takeoff(4)
+    print("Setting airspeed")
+    vehicle.airspeed = 3
+    cur_lat = 0
+    cur_long = 0
+    cur_hgt = 0
+    while True:
+        print((latd),lat_to_x(latd),(longd),long_to_x(longd),hght)
+        if cur_lat!=latd or cur_long!=longd or cur_hgt!=hght:
+            point1 = LocationGlobalRelative(latd, longd, hght)
+            vehicle.simple_goto(point1)
+            cur_lat = latd
+            cur_long = longd
+            cur_hgt = hght
+        time.sleep(0.1)
+
+        
+
+	# print("Going towards first point for 30 seconds ...")
+	# point1 = LocationGlobalRelative(-35.3632591, 149.1653452,10)
+	# vehicle.simple_goto(point1)
+	# print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
+	# print("\n\n\nhello i am here here here ",vehicle.location.global_relative_frame.alt,"\n\n\n\n\n")
+	
+	# print("\n\n\nhello i am here here here ",vehicle.location.global_relative_frame.alt,"\n\n\n\n\n")
